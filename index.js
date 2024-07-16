0c077f6aeeeaa7c4d9908782f8c410e10497333a
Ilia Kani <kanishchev.ilia@gmail.com>
2024-07-16 13:41:25 +0200

    made refactoring

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const aboutLink = document.querySelector('a[href="#about-me"]');
-aboutLink.addEventListener('click', function(e) {
-    e.preventDefault();
-    const about = document.querySelector('#about-me');
-    about.scrollIntoView({ behavior: 'smooth' });
-});
+document.addEventListener('DOMContentLoaded', () => {
+    const aboutLink = document.querySelector('a[href="#about-me"]');
+    aboutLink.addEventListener('click', (e) => {
+        e.preventDefault();
+        const about = document.querySelector('#about-me');
+        about.scrollIntoView({ behavior: 'smooth' });
+    });
 
-document.addEventListener('DOMContentLoaded', function() {
     const emailElement = document.querySelector('#copyemail');
-    emailElement.addEventListener('click', function() {
-        const email = this.getAttribute('data-clipboard-text');
-        navigator.clipboard.writeText(email).then(function() {
+    emailElement.addEventListener('click', async () => {
+        const email = emailElement.getAttribute('data-clipboard-text');
+        try {
+            await navigator.clipboard.writeText(email);
             console.log('Email copied to clipboard!');
-        }).catch(function(error) {
+        } catch (error) {
             console.error('Error copying email to clipboard', error);
-        });
+        }
     });
 });
